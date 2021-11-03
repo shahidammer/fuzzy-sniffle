0f1b143cbe8b43b5b56b9900826cb8983f66d00b
shahidammer <shahidammer@gmail.com>
2021-11-03 21:31:03 +0500

    working spark

--- kafka/twitter_producer.py
+++ kafka/twitter_producer.py
@@ -1,24 +1,35 @@
 from tweepy import OAuthHandler,Stream
-import tweepy,json
+import json
 
 from pykafka import KafkaClient
 
 import sys
-import os
+from os import getenv
 from pathlib import Path
 from dotenv import load_dotenv
-
 load_dotenv(dotenv_path=Path('../', '.env'))
 
-class TweetListener(tweepy.StreamListener):
-	def __init__(self,api=None):
-		super(TweetListener,self).__init__()
-		self.client = KafkaClient("127.0.0.1:9092")
-		self.producer = self.client.topics[bytes('twitter','ascii')].get_producer()
-	def on_status(self,status):		
+CONSUMER_KEY = getenv('CONSUMER_KEY')
+CONSUMER_SECRET = getenv('CONSUMER_SECRET')
+ACCESS_TOKEN = getenv('ACCESS_TOKEN')
+ACCESS_SECRET = getenv('ACCESS_SECRET')
+BROKER_LIST = ",".join(getenv('BROKER_LIST').split(";"))
+KAFKA_TOPIC = getenv('KAFKA_TOPIC')
+
+kafka_client = KafkaClient(BROKER_LIST)
+p = kafka_client.topics[bytes(KAFKA_TOPIC,'ascii')].get_producer()
+
+def on_send_success(msg):
+	print('yyyy')
+def on_send_error(excp):
+	print("nnn")
+class TweetListener(Stream):
+	def on_status(self,status):				
 		try:
 			tweet=status._json
-			self.producer.produce(bytes(json.dumps(tweet),'ascii'))
+			print(f"Created at: {tweet['created_at']} Tweet {tweet['id']}")
+			text = ",".join(list(tweet.keys()))
+			p.produce(bytes(text,'ascii'))
 		except Exception as e:
 			print(e)
 	def on_error(self, status):
@@ -28,15 +39,7 @@ class TweetListener(tweepy.StreamListener):
 def main():
 	query_string = ["BTC","BITCOIN"]
 	
-	CONSUMER_KEY = os.getenv('CONSUMER_KEY')
-	CONSUMER_SECRET = os.getenv('CONSUMER_SECRET')
-	ACCESS_TOKEN = os.getenv('ACCESS_TOKEN')
-	ACCESS_SECRET = os.getenv('ACCESS_SECRET')
-
-	auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-	auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
-	l = TweetListener()
-	twitter_stream = tweepy.Stream(auth, l)
+	twitter_stream = TweetListener(CONSUMER_KEY, CONSUMER_SECRET,ACCESS_TOKEN,ACCESS_SECRET)
 	twitter_stream.filter(languages=['en'], track=query_string)
 
 if __name__ == "__main__":
--- spark/spark_twitter.py
+++ spark/spark_twitter.py
@@ -1,19 +1,34 @@
-import sys
-from pyspark import SparkContext
-from pyspark.streaming import StreamingContext
-from pyspark.streaming.kafka import KafkaUtils
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import *
+from pyspark.sql.types import *
+import math
+import string
+import random
+import json
+
+KAFKA_INPUT_TOPIC_NAME_CONS = "tweets"
+KAFKA_BOOTSTRAP_SERVERS_CONS = "localhost:9092"
 
 if __name__ == "__main__":
-    sc = SparkContext(appName="PythonStreamingDirectKafkaWordCount")
-    ssc = StreamingContext(sc, 2)
-    brokers, topic = 'localhost:9092', 'twitter'
-    kvs = KafkaUtils.createDirectStream(ssc, [topic],{"metadata.broker.list": brokers})
-    lines = kvs.map(lambda x: x[1])
-    counts = lines.flatMap(lambda line: line.split(" ")) \
-                  .map(lambda word: (word, 1)) \
-                  .reduceByKey(lambda a, b: a+b)
-    counts.pprint()
-    ssc.start()
-    ssc.awaitTermination()
+    spark = SparkSession \
+    .builder \
+    .appName("PySpark Structured Streaming with Kafka") \
+    .master("local[*]") \
+    .getOrCreate()
+    
+    print("PySpark Structured Streaming with Kafka Application Started …")
+    # Construct a streaming DataFrame that reads from testtopic
+    df = spark \
+    .readStream \
+    .format("kafka") \
+    .option("kafka.bootstrap.servers", KAFKA_BOOTSTRAP_SERVERS_CONS) \
+    .option("subscribe", KAFKA_INPUT_TOPIC_NAME_CONS) \
+    .option("startingOffsets", "earliest") \
+    .load()
+    # df = df.selectExpr("CAST(key AS STRING)","CAST(value AS STRING)", "timestamp").collect()
+    
+    query = df.writeStream.format("console").start()
 
-# $SPARK_HOME/bin/spark-submit --packages org.apache.spark:spark-streaming-kafka-0-8_2.11:2.4.4 spark_twitter.py
+    query.awaitTermination()
+
+# spark-submit --packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.2.0 spark_twitter.py 
