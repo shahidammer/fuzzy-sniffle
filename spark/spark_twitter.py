1c975f99c1b59b2d605246806bcb76ec54c16851
shahidammer <shahidammer@gmail.com>
2021-11-04 17:59:05 +0500

    working app

--- spark/spark_twitter.py
+++ spark/spark_twitter.py
@@ -27,8 +27,9 @@ if __name__ == "__main__":
         .master("local[*]") \
         .getOrCreate()
     
+    spark.sparkContext.setLogLevel("ERROR")
+
     print("PySpark Structured Streaming with Kafka Application Started")
-    
     # Construct a streaming DataFrame that reads from testtopic
     df = spark \
         .readStream \
@@ -38,7 +39,8 @@ if __name__ == "__main__":
         .option("startingOffsets", "earliest") \
         .load()
     df = df.selectExpr("CAST(value AS STRING)")
-
+    print(f"Streaming DataFrame : {df.isStreaming}" )
+    
     structureSchema = StructType([
         StructField('created_at', StringType(), True),
         StructField('id', StringType(), True),
@@ -75,7 +77,46 @@ if __name__ == "__main__":
 
     tweets = df.select(from_json(col("value").cast("string"), structureSchema).alias("tweets")).select("tweets.*")
     tweets.printSchema()
-
-    tweets.writeStream.outputMode("append").format("console").start().awaitTermination()
     
 
+
+
+    tweets = tweets.withColumnRenamed("id", "tweet_id") \
+        .withColumnRenamed("created_at", "tweet_created_at") \
+        .withColumnRenamed("text", "tweet_text") \
+
+
+    user_df = tweets.select("tweet_id","tweet_created_at","tweet_text", "user.*")
+    def replace_names(prefix,x):
+        if x.startswith("tweet_") or x.startswith("rt_"):
+            return x
+        return f"{prefix}_{x}"
+
+
+    new_colnames= list(map(lambda x: replace_names("user",x), user_df.columns))
+    user_df = user_df.toDF(*new_colnames)  
+
+    
+    rt_df = tweets \
+        .select("tweet_id", "retweeted_status.id","retweeted_status.text","retweeted_status.user") \
+        .withColumnRenamed("id", "rt_id") \
+        .withColumnRenamed("text", "rt_text") \
+        .select("tweet_id", "rt_id","rt_text","user.*") 
+
+    new_colnames= list(map(lambda x: replace_names('rt_user',x), rt_df.columns))
+    rt_df = rt_df.toDF(*new_colnames)
+
+    final_df = user_df.join(rt_df, (user_df.tweet_id == rt_df.tweet_id) & (user_df.tweet_id == rt_df.tweet_id))
+
+    final_df \
+    .writeStream \
+    .outputMode("append") \
+    .trigger(processingTime='60 seconds') \
+    .format("csv") \
+    .option("path", "output/filesink_output") \
+    .option("header", True) \
+    .option("checkpointLocation", "checkpoint/filesink_checkpoint") \
+    .start() \
+    .awaitTermination()
+
+   
